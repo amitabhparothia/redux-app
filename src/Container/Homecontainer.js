f00f149bae76078212d96e946f9dcea9b3bffa22
Amitabh Parothia <amitabhparothia438@gmail.com>
2024-03-22 00:19:53 +0530

    create container and change the name of actions file into action.js

--- src/Container/Homecontainer.js
+++ src/Container/Homecontainer.js
@@ -1,9 +1,13 @@
-import React from "react"
+import { Connect } from "react-redux"
 import { Home } from "../Components/Home"
-export const Homecontainer = () => {
-    return(
-        <>
-            <Home />
-        </>
-    )
-}
+import { addtocart } from "../Services/Actions/action"
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(mapStateToProps , mapDispatchToProps)(Home)
--- src/Services/Actions/action.js
+++ src/Services/Actions/action.js
@@ -1,5 +1,5 @@
 import {ADD_TO_CART} from '../Constants'
-export const action = (data) => {
+export const addtocart = (data) => {
     return{
         type:ADD_TO_CART,
         data:data
